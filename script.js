9de26bc9c2a83ff2d40bb26cc950327c1b7a49bd
Daniel Hyman <dahyman91@gmail.com>
2021-09-05 15:51:48 -0400

    created mobile design

---
+++ script.js
@@ -0,0 +1,56 @@
+const items = document.querySelectorAll("img");
+const itemCount = items.length;
+const nextItem = document.querySelector(".next");
+const previousItem = document.querySelector(".previous");
+const currImg = document.querySelector(".slider");
+const homeCurrImg = document.querySelector(".slider-home");
+const imageDescriptions = document.querySelectorAll(".img-text");
+
+//Slider Functionality
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove("active");
+  if (count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+  items[count].classList.add("active");
+  imageDescriptions[count]?.classList.remove("hidden");
+  imageDescriptions[count + 1]?.classList.add("hidden");
+  imageDescriptions[count - 1]?.classList.add("hidden");
+}
+
+function showPreviousItem() {
+  items[count].classList.remove("active");
+
+  if (count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+  items[count].classList.add("active");
+  imageDescriptions[count]?.classList.remove("hidden");
+  imageDescriptions[count + 1]?.classList.add("hidden");
+  imageDescriptions[count - 1]?.classList.add("hidden");
+}
+
+function keyPress(e) {
+  e = e || window.event;
+  if (e.keyCode == "37") {
+    showPreviousItem();
+  } else if (e.keyCode == "39") {
+    showNextItem();
+  }
+}
+
+// Show text
+function showNextText() {
+  imageDescriptions[count].classList.remove("hidden");
+}
+
+//Event Handlers
+nextItem.addEventListener("click", showNextItem);
+previousItem.addEventListener("click", showPreviousItem);
+homeCurrImg.addEventListener("click", showNextItem);
